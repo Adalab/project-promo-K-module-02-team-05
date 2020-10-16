e8754a3b843da744891c10ed2b9a2327dc1e5ee3
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-16 19:22:08 +0200

    fix javascript into form

--- src/js/form.js
+++ src/js/form.js
@@ -1,9 +1,33 @@
 'user strict';
-const inputList = document.querySelectorAll('.js-fill__input');
-// const cardValue = document.querySelectorAll('.js__card--value');
+// const inputList = document.querySelectorAll('.js-fill__input');
+// // const cardValue = document.querySelectorAll('.js__card--value');
+// let data = {
+//   fullName: 'Barry Allen - Flash',
+//   job: 'The fastest man alive',
+//   mail: '',
+//   telephone: '',
+//   linkedin: '',
+//   github: '',
+// };
+// const saveField = function (event) {
+//   data[event.currentTarget.id] = event.currentTarget.value;
+//   console.log(data[event.currentTarget.id]);
+//   console.log(event.currentTarget.id);
+//   const idCards = `#js-${event.currentTarget.id}-card`;
+//   document.querySelector(idCards).innerHTML = event.currentTarget.value;
+//   console.log(idCards);
+// };
+// //
+// for (const eachElement of inputList) {
+//   eachElement.addEventListener('blur', saveField);
+// }
+const inputTitle = document.querySelectorAll('.js-fill__inputTitle');
+const inputLink = document.querySelectorAll('.js-fill__inputLink');
 let data = {
-  fullName: 'Barry Allen - Flash',
-  job: 'The fastest man alive',
+  fullName: '',
+  job: '',
+};
+  let info = {
   mail: '',
   telephone: '',
   linkedin: '',
@@ -11,13 +35,17 @@ let data = {
 };
 const saveField = function (event) {
   data[event.currentTarget.id] = event.currentTarget.value;
-  console.log(data[event.currentTarget.id]);
-  console.log(event.currentTarget.id);
   const idCards = `#js-${event.currentTarget.id}-card`;
-  document.querySelector(idCards).innerHTML = event.currentTarget.value;
-  console.log(idCards);
+    document.querySelector(idCards).innerHTML = event.currentTarget.value;
 };
-//
-for (const eachElement of inputList) {
-  eachElement.addEventListener('blur', saveField);
+const saveLink = function (event) {
+  info[event.currentTarget.id] = event.currentTarget.value;
+  const idLinks = `#js-${event.currentTarget.id}-link`;
+    document.querySelector(idLinks).href = event.currentTarget.value;
+};
+for (const eachElement of inputTitle) {
+  eachElement.addEventListener('keyup', saveField);
+}
+for (const eachElement of inputLink) {
+  eachElement.addEventListener('keyup', saveLink);
 }
