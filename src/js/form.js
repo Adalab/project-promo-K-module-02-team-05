984b7c8a7037f7d123f76401ce913305f051d05f
Eleonora Dell'Amico <eleonoradellamico4@gmail.com>
2020-10-20 13:49:29 +0200

    share button

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,4 @@
-'user strict';
+'use strict';
 const inputList = document.querySelectorAll('.js-fill__input');
 // const cardValue = document.querySelectorAll('.js__card--value');
 let data = {
@@ -17,12 +17,22 @@ const saveField = function (event) {
   // document.querySelector(idCards).innerHTML = event.currentTarget.value;
   const el = document.querySelector(idCards);
   if (el.nodeName !== 'A') {
-   el.innerHTML = event.currentTarget.value;
+    el.innerHTML = event.currentTarget.value;
   } else {
-   el.href = event.currentTarget.value;
+    el.href = event.currentTarget.value;
   }
 }
 //
 for (const eachElement of inputList) {
   eachElement.addEventListener('input', saveField);
 }
+
+const shareLink = document.querySelector('.js-link');
+const btnTwitter = document.querySelector('.js-share');
+const clickShare = document.querySelector(".js-share__button");
+const openTwitter = function (event) {
+  event.preventDefault();
+  shareLink.classList.add("js__show");
+  clickShare.classList.add("shareSent");
+};
+btnTwitter.addEventListener('click', openTwitter);
