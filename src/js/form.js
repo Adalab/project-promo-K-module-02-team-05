9224557056b5b7be1af04c09d8d11aac347a7f30
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-21 14:45:09 +0200

    add reset button

--- src/js/form.js
+++ src/js/form.js
@@ -1,9 +1,8 @@
 'use strict';
 const inputList = document.querySelectorAll('.js-fill__input');
-// const cardValue = document.querySelectorAll('.js__card--value');
 let data = {
-  fullName: 'Barry Allen - Flash',
-  job: 'The fastest man alive',
+  fullName: '',
+  job: '',
   mail: '',
   telephone: '',
   linkedin: '',
@@ -11,31 +10,29 @@ let data = {
 };
 const saveField = function (event) {
   data[event.currentTarget.id] = event.currentTarget.value;
-  const idCards = `#js-${event.currentTarget.id}-card`;
-  // document.querySelector(idCards).innerHTML = event.currentTarget.value;
-  const el = document.querySelector(idCards);
-  if (el.nodeName !== 'A') {
-    el.innerHTML = event.currentTarget.value;
-  } else {
-    el.href = event.currentTarget.value;
-  }
-  // render();
+  render();
 }
 
-// function render(event){
-//   const idCards = `#js-${event.currentTarget.id}-card`;
-//   // document.querySelector(idCards).innerHTML = event.currentTarget.value;
-//   const el = document.querySelector(idCards);
-//   if (el.nodeName !== 'A') {
-//     el.innerHTML = event.currentTarget.value;
-//   } else {
-//     el.href = event.currentTarget.value;
-//   }
-// }
-//
+function render(event){
+  document.querySelector("#js-fullName-card").innerHTML = data.fullName
+  || "Barry Allen - Flash" ;
+  document.querySelector("#js-job-card").innerHTML = data.job
+  || "The Fastest Men Alive" ;
+  document.querySelector("#js-telephone-card").href = data.telephone || "#"
+  document.querySelector("#js-mail-card").href = data.mail
+  || "#" ;
+  document.querySelector("#js-linkedin-card").href = data.linkedin
+  || "#" ;
+  document.querySelector("#js-github-card").href = data.github
+  || "#" ;
+
+}
+
 for (const eachElement of inputList) {
   eachElement.addEventListener('input', saveField);
 }
+
+
 
 const shareLink = document.querySelector('.js-link');
 const btnTwitter = document.querySelector('.js-share');
@@ -46,3 +43,22 @@ const openTwitter = function (event) {
   clickShare.classList.add("shareSent");
 };
 btnTwitter.addEventListener('click', openTwitter);
+
+
+const btnReset = document.querySelector(".js-sectionOneBtn");
+
+function resetForm(){
+  for(const input of inputList){
+    input.value = "";
+  }
+  data.fullName = "";
+  data.job = "";
+  data.telephone = "";
+  data.mail = "";
+  data.linkedin = "";
+  data.github = "";
+  render();
+}
+
+btnReset.addEventListener("click", resetForm);
+render();
